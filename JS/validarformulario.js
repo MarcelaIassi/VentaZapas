429c1270fb249861bbed5bf7c8f3b5c3900585bf
Marcela <iassi.marce@gmail.com>
2021-10-21 16:02:33 -0300

    Subiendo cambios

--- JS/validarformulario.js
+++ JS/validarformulario.js
@@ -1,14 +1,31 @@
-var nombre = document.getElementById("nombre");
-var error = document.getElementById("error");
+window.onload = enviarFormulario;
 
 
+/*var error = document.getElementById("error");*/
 function enviarFormulario(){
-    console.log("Enviando formulario....");
-    var mensajesError = [];
-
-    if(nombre.value === " "){
-       mensajesError.push("Ingrese su Nombre");
-    }
-    error.innerHTML = mensajesError.join(" , ");
-    return false;
+    document.getElementById("enviar").addEventListener('click',validar,false);
 }
+
+function validaApellido(){
+    var apellido = document.getElementById("apellido");
+    if(apellido.value == " "){
+       alert("Ingrese su Apellido");
+       return false;
+    }
+    return true;
+}
+function validar(e){
+    if(validaApellido() && confirm("Pulsa Enviar si deseas enviar tus datos")){
+        return true;
+    }else{
+        e.preventDefault();
+        return false;
+    }
+}
+
+
+
+/*console.log("Enviando formulario....");
+var mensajesError = [];
+error.innerHTML = mensajesError.join(" , ");
+return false;*/
