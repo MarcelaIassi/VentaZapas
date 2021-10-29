45e8eaac1321050c090af28489b6a6bf4a30b992
Marcela <iassi.marce@gmail.com>
2021-10-29 18:27:24 -0300

    Cambios

--- JS/validarformulario.js
+++ JS/validarformulario.js
@@ -45,36 +45,3 @@
 }())
 
 
-
-
-
-  
-
-
-//window.onload = enviarFormulario;
-
-
-/*var error = document.getElementById("error");*/
-//function enviarFormulario(){
-//    document.getElementById("enviar").addEventListener('click',validar,false);
-//}
-
-//function validaApellido(){
-//    var apellido = document.getElementById("apellido");
-//    if(apellido.value == " "){
-//       alert("Ingrese su Apellido");
-//       return false;
-//    }
-//    return true;
-//}
-//function validar(e){
-//    if(validaApellido() && confirm("Pulsa Enviar si deseas enviar tus datos")){
-//        return true;
-//    }else{
-//        e.preventDefault();
-//        return false;
-//    }
-//}
-
-
-
